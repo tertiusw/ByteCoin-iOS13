88a62ee973085e2037adaa983c81bc31e73e1bb9
tertiusw <tertius.wessels@gmail.com>
2024-06-11 13:21:44 +0200

    BitCoinPricing

--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -7,13 +7,61 @@
 //
 
 import Foundation
+protocol CoinManagerDelegate
+{
+    func didUpdatePrice(price: String, currency: String)
+    func didFailWithError(error: Error)
+}
 
-struct CoinManager {
+struct CoinManager
+{
+    var delegate: CoinManagerDelegate?
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
+    let apiKey = "8EAFB667-0DE3-4F42-BB61-26F439725070"
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
 
+    func getCoinPrice(for currency: String)
+    {
+        let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
+        if let url = URL(string: urlString)
+        {
+            let session = URLSession(configuration:.default)
+            let task = session.dataTask(with: url) {(data, response, error) in
+                if error != nil
+                {
+                    print(error!)
+                    return
+                }
+                if let safeData = data
+                {
+                    if let bitcoinPrice = self.pareJSON(safeData)
+                    {
+                        let priceString = String(format: "%.2f", bitcoinPrice)
+                        self.delegate?.didUpdatePrice(price: priceString, currency: currency)
+                    }
+                }
+                
+            }
+            task.resume()
+        }
+    }
     
+    func pareJSON(_ data: Data) -> Double?
+    {
+        let decoder = JSONDecoder()
+        do
+        {
+            let decodedData = try decoder.decode(CoinData.self, from: data)
+            let lastPrice = decodedData.rate
+            print(lastPrice)
+            return lastPrice
+        }
+        catch
+        {
+            print(error)
+            return nil
+        }
+    }
 }
